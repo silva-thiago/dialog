2d2f7823e5f35b8ae6b091b88d59d660381aeb52
Thiago Silva <thiago.silva@imd.ufrn.br>
2019-01-18 20:12:04 -0300

    Criando o projeto, setState, spread operator, renderizando uma lista de componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Comments from './Comments'
+import NewComment from './NewComment'
 import './scss/App.css'
 
 class App extends Component {
+  state = {
+    comments: [
+      'Comment 1',
+      'Comment 2',
+      'Comment 3',
+      'Comment 4'
+    ]
+  }
+
+  sendComment = comment => {
+    this.setState({
+      comments: [...this.state.comments, comment],
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <h3>Dialog</h3>
         </header>
+        
+        <div className='container'>
+          <NewComment sendComment={this.sendComment} />
+          <Comments comments={this.state.comments} />
+        </div>
       </div>
     )
   }
 }
 
-export default App;
+export default App
