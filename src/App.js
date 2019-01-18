0c860bb2e367d72263302a86b3182f19c8fc4512
Thiago Silva <thiago.silva@imd.ufrn.br>
2019-01-18 17:48:43 -0300

    Remoção de caracteres realizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './scss/App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './scss/App.css'
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
           <h3>Dialog</h3>
         </header>
       </div>
-    );
+    )
   }
 }
 
